677a16742fd49a8e48d67698b43faeaa6da24f75
F.L.o <43219073+Nairolf00@users.noreply.github.com>
2018-09-16 00:04:15 +0200

    added speed slider

--- http_train.ino
+++ http_train.ino
@@ -66,12 +66,20 @@ void handleRoot() {
     server.send(200, "text/html",
       "<html>\
         <head>\
+          <meta charset=\"utf-8\">\
           <title>TRAIN (ESP8266)</title>\
           <style>\
             body { background-color: #000000; font-family: Arial, Helvetica, Sans-Serif; Color: #ffffff; text-align: center; user-select: none;}\
-            a:link.on_Link , a:visited.on_Link {background-color: #43f436; color: black; padding: 14px 25px; text-align: center; text-decoration: none; display: inline-block;}\
-            a:link.off_Link, a:visited.off_Link {background-color: #f44336; color: white; padding: 14px 25px; text-align: center; text-decoration: none; display: inline-block;}\
-            a:link.neut_Link, a:visited.neut_Link {background-color: #f4f4f4; color: black; padding: 14px 25px; text-align: center; text-decoration: none; display: inline-block;}\
+            p {margin: 0px;}\
+            .slidecontainer {width: 50%; margin: 0 auto; background-color: #000000;}\
+            .slider {-webkit-appearance: none; width: 100%; height: 50px; outline: none; background-color: #000000}\
+            .slider::-webkit-slider-thumb {-webkit-appearance: none; appearance: none; height: 45px; width: 65px; background: #f4f4f4; border-radius: 0%; outline-style: none; cursor: pointer; -webkit-transition: .1s;}\
+            .slider:hover::-webkit-slider-thumb {background: #BFBFBF;}\
+            .slider::-moz-range-thumb { height: 45px; width: 65px;  background: #f4f4f4; border-radius: 0%; border: none; cursor: pointer; transition: background .1s; }\
+            .slider:hover::-moz-range-thumb {background: #BFBFBF;}\
+            a:link.on_Link , a:visited.on_Link {background-color: #43f436; color: black; width: 40px; padding: 14px 14px; text-align: center; text-decoration: none; display: inline-block; -webkit-transition: .1s; transition: background-color .1s;}\
+            a:link.off_Link, a:visited.off_Link {background-color: #f44336; color: white; padding: 14px 14px; width: 40px; text-align: center; text-decoration: none; display: inline-block; -webkit-transition: .1s; transition: background-color .1s;}\
+            a:link.neut_Link, a:visited.neut_Link {background-color: #f4f4f4; color: black; padding: 14px 14px; width: 40px; text-align: center; text-decoration: none; display: inline-block; -webkit-transition: .1s; transition: background-color .1s;}\
             a:hover.on_Link , a:active.on_Link {background-color: #35BF2A;}\
             a:hover.off_Link, a:active.off_Link {background-color: #BF352A;}\
             a:hover.neut_Link, a:active.neut_Link {background-color: #BFBFBF;}\
@@ -82,37 +90,71 @@ void handleRoot() {
           <p>\
             </br> timeout=   "+ String(timeout) +" speed=<b id=\"speed\">" + String(speed) + "</b>   dir=<b id=\"direction\">"+ dir_text[direction] +"</b>\
             </br></br>\
-            <a href=\"#ccw\" class=\"neut_Link\" data-command=\"d\" data-target=\"/?d=1\">CCW</a>\
-            <a href=\"#cw\" class=\"neut_Link\" data-command=\"d\" data-target=\"/?d=0\">CW</a>\
-            </br></br>\
-            <a href=\"#off\" class=\"off_Link\" data-command=\"s\" data-target=\"/?c=-100\">off</a>\
-            <a href=\"#dm\" class=\"off_Link\" data-command=\"s\" data-target=\"/?c=-10\">--</a>\
-            <a href=\"#m\" class=\"off_Link\" data-command=\"s\" data-target=\"/?c=-1\">-</a>\
-            <a href=\"#p\" class=\"on_Link\" data-command=\"s\" data-target=\"/?c=+1\">+</a>\
-            <a href=\"#dp\" class=\"on_Link\" data-command=\"s\" data-target=\"/?c=+10\">++</a>\
-            <a href=\"#max\" class=\"on_Link\" data-command=\"s\" data-target=\"/?c=+100\">full</a>\
+            <a href=\"#ccw\" class=\"neut_Link\" data-command=\"d\" data-target=\"1\">CCW</a>\
+            <a href=\"#cw\" class=\"neut_Link\" data-command=\"d\" data-target=\"0\">CW</a>\
           </p>\
+          <div class=\"slidecontainer\">\
+            <input type=\"range\" min=\"0\" max=\"100\" value=\"50\" class=\"slider\" id=\"myRange\">\
+          </div>\
+          <p>\
+            <a href=\"#off\" class=\"off_Link\" data-command=\"s\" data-target=\"-100\">off</a>\
+            <a href=\"#dm\" class=\"off_Link\" data-command=\"s\" data-target=\"-10\">--</a>\
+            <a href=\"#m\" class=\"off_Link\" data-command=\"s\" data-target=\"-1\">-</a>\
+            <a href=\"#p\" class=\"on_Link\" data-command=\"s\" data-target=\"+1\">+</a>\
+            <a href=\"#dp\" class=\"on_Link\" data-command=\"s\" data-target=\"+10\">++</a>\
+            <a href=\"#max\" class=\"on_Link\" data-command=\"s\" data-target=\"+100\">full</a>\
+          </p>\
+          </br></br>\ 
           <script type=\"text/javascript\">\
+            var slider = document.getElementById(\"myRange\");\
+            var test;\
+            slider.oninput = function() {\
+              let sliderspeed = this.value;\
+              clearTimeout(test);\
+              test = setTimeout(function(){sendrequest(sliderspeed, 0);}, 50);\
+            }\
+          </script>\
+          <script type=\"text/javascript\">\
+            let request = new XMLHttpRequest();\
+            function sendrequest(value, whichvalue){\
+              console.log(value);\
+              request.addEventListener('load', function(event) {\
+                if (request.status >= 200 && request.status < 300) {\
+                  if (whichvalue == 0 || whichvalue == 1){\
+                    response = request.responseText;\
+                    document.getElementById(\"speed\").innerHTML = response;\
+                    slider.value = response;\
+                  } else if (whichvalue == 2){\
+                    document.getElementById(\"direction\").innerHTML = request.responseText;\
+                  }\
+                } else {\
+                  console.warn(request.statusText, request.responseText);\
+                }\
+              });\
+              switch(whichvalue){\
+                case 0:\
+                  request.open(\"GET\", \"/?s=\"+String(value));\
+                break;\
+                case 1:\
+                  request.open(\"GET\", \"/?c=\"+String(value));\
+                break;\
+                case 2:\
+                  request.open(\"GET\", \"/?d=\"+String(value));\
+                break;\
+              }\
+              request.send();\
+            }\
             window.onload = function() {\
               let links = document.getElementsByTagName(\"a\");\
               for(let index = 0; index < links.length; index++) {\
                 links[index].addEventListener(\"click\", (event) => {\
                   if(event.target != null) {\
                     let command = event.target.dataset.command;\
-                    let request = new XMLHttpRequest();\
-                    request.addEventListener('load', function(event) {\
-                      if (request.status >= 200 && request.status < 300) {\
-                        if(command == \"s\") {\
-                          document.getElementById(\"speed\").innerHTML = request.responseText;\
-                        } else if(command == \"d\") {\
-                          document.getElementById(\"direction\").innerHTML = request.responseText;\
-                        }\
-                      } else {\
-                         console.warn(request.statusText, request.responseText);\
-                      }\
-                    });\
-                    request.open(\"GET\", event.target.dataset.target);\
-                    request.send();\
+                    if(command = \"s\"){\
+                      sendrequest(event.target.dataset.target, 1);\
+                    } else if (command = \"d\"){\
+                      sendrequest(event.target.dataset.target, 2);\
+                    }\
                   }\
                 });\
               }\
